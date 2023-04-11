b993498f9c378cac593d58c6d142442be5e234e7
Juan Rolo <juanluis211@gmail.com>
2023-04-11 23:09:42 +0200

    Change format and tailwind config

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,7 @@
-import React from 'react'
 import './Footer.scss'
 
-const Footer = () => {
-  return (
-    <div className='footer'>Footer</div>
-  )
+function Footer() {
+  return <div className="footer">Footer</div>
 }
 
 export default Footer

ff62f39026869b81080b314b162e9dca3e6aa8c1
Carlos Daniel Dominguez <carlosddominguezc@gmail.com>
2023-08-09 07:18:05 -0500

    Changed header´s background

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,13 +18,13 @@ function Header() {
   let [open, setOpen] = useState(false)
 
   return (
-    <header className="fixed left-0 top-0 z-10 w-full bg-neutral-100 text-center text-neutral-600 shadow-lg dark:bg-neutral-600 dark:text-neutral-200 lg:text-left">
+    <header className="left-0 top-0 z-10 h-screen w-full bg-fixed bg-no-repeat bg-cover bg-[url('https://images.pexels.com/photos/683039/pexels-photo-683039.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] text-center text-neutral-600 shadow-lg dark:bg-neutral-600 dark:text-neutral-200 lg:text-left">
       <div className="items-center justify-between px-7 pb-2 md:flex md:px-10">
         <div className="flex cursor-pointer items-center text-2xl font-bold">
           <span className="mr-1 pt-2 text-3xl">
             <img
               className="h-20 w-40"
-              src="https://i.imgur.com/Ayu94vl.png"
+              src="https://i.imgur.com/f8IEWXq.png"
               alt="Logo"
             />
           </span>
@@ -39,11 +39,11 @@ function Header() {
           <ion-icon name={open ? 'close' : 'menu'} />
         </div>
         <ul
-          className={`absolute left-0 z-[-1] w-full bg-neutral-100 pb-12 pl-1 dark:bg-neutral-600 dark:text-neutral-200 md:static md:mt-5 md:z-auto md:flex md:w-auto md:items-center md:pb-0 md:pl-0 ${open ? 'top-30 opacity-100' : 'top-[-490px]'
+          className={`absolute left-0 z-[-1] w-full bg-none pb-12 pl-1 text-neutral-200 dark:bg-neutral-600 dark:text-neutral-200 md:static md:mt-5 md:z-auto md:flex md:w-auto md:items-center md:pb-0 md:pl-0 ${open ? 'top-30 opacity-100' : 'top-[-490px]'
             } opacity-0 md:opacity-100 `}
         >
           {links.map((link) => (
-            <li key={link.name} className="my-7 text-xl md:my-0 md:ml-8">
+            <li key={link.name} className="my-7 text-xl font-medium md:my-0 md:ml-8">
               <Link to={link.link} className=" hover:text-green">
                 {link.name}
               </Link>
@@ -62,6 +62,11 @@ function Header() {
             </button>
           </li>
         </ul>
+      </div>
+      <div>
+        <h1 className="text-8xl text-neutral-200 ml-40 mt-40">Fans Coffee <br/>
+        Bakery</h1>
+          <p className="text-neutral-200 font-semibold ml-40 mt-10">Donde tus antojos soñados,<br />cobran realidad...</p>
       </div>
     </header>
   )
